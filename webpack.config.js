0db5c48b64c4a2a086a9b1fe810f97e12447056b
UdayDocs <137787964+UdayDocs@users.noreply.github.com>
2025-01-28 15:37:27 +0530

    Prepare for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,64 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const webpack = require('webpack');
-const API_KEY = process.env.API_KEY || 'fallback_key_for_development';
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const webpack = require("webpack");
+const API_KEY = process.env.API_KEY || "fallback_key_for_development";
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
     clean: true,
-    publicPath: '/',
+    publicPath: "/",
   },
   resolve: {
     fallback: {
-       path: false,
+      path: false,
       os: false,
       crypto: false,
       stream: false,
       buffer: false,
       vm: false,
-      fs: false  
+      fs: false,
     },
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/template.html',
+      template: "./src/template.html",
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
+      filename: "styles.[contenthash].css",
     }),
-        new webpack.DefinePlugin({
-      'process.env.API_KEY': JSON.stringify(btoa(API_KEY).split('').reverse().join(''))
+    new webpack.DefinePlugin({
+      "process.env.API_KEY": JSON.stringify(
+        btoa(API_KEY).split("").reverse().join("")
+      ),
     }),
   ],
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/images/[name][ext]',
+          filename: "assets/images/[name][ext]",
         },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/fonts/[name][ext]',
+          filename: "assets/fonts/[name][ext]",
         },
       },
     ],
@@ -69,12 +66,12 @@ module.exports = {
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(),
-      '...', // Keep default JS minimizer
+      "...", // Keep default JS minimizer
     ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     hot: true,
     historyApiFallback: true,
