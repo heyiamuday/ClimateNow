2dfdda1fbac6d9743eb654809060e1b3a76dbfa6
UdayDocs <137787964+UdayDocs@users.noreply.github.com>
2025-01-28 21:37:10 +0530

    Updated package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
     clean: true,
-    publicPath: "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/ClimateNow/" : "/",
   },
   resolve: {
     fallback: {
